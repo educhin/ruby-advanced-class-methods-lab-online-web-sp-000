caec930b73f2ed71de208fe10ec6cc79759f73e7
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-14 00:51:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,4 +59,13 @@ class Song
     song.artist_name = data[0]
     song
   end
+
+  def self.create_from_filename(file)
+    data = file.split(" - ")
+    name = data[1].split(".")[0]
+    song = self.new_by_name(name)
+
+    song.artist_name = data[0]
+    song
+  end
 end
